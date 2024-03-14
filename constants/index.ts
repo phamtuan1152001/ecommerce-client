e5b4f7cf7683088fbabfcb5931025c9b22faa4a2
phamtuan11520011 <phamtuan1152001@gmail.com>
2024-03-14 17:44:45 +0700

    UPDATE API DEPLOY

--- constants/index.ts
+++ constants/index.ts
@@ -1,6 +1,7 @@
 import NO_IMG from "@/public/assets/images/empty-data/no-data-image.jpg"
 
 export const BASE_URL_API_DEV = "http://localhost:3002"
+// export const BASE_URL_API_DEV = "https://ecommerce-ptuanstore-api.onrender.com"
 export const URL_DEV_VNAPPMOB = "https://vapi.vnappmob.com/api"
 
 /* PRODUCTS API */
